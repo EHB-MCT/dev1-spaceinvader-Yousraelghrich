0fd4436445b1fd2a00ce23e98ddc97d4e51d2e04
yousra <yousra.el.ghrich@student.ehb.be>
2022-10-11 23:06:23 +0200

    opdracht gemaakt

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,52 @@
 "use strict";
 
-alert("Draw your space invader here");
+drawline();
+
+function drawline() {
+    let canvas = document.querySelector("canvas")
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d");
+ 
+    context.beginPath();
+    context.fillStyle = "PINK";
+    context.rect(50,50,100,200);
+    context.fill();
+    context.stroke();
+
+
+    context.beginPath();
+    context.fillStyle = "PINK";
+    context.rect(50,350,100,100);
+    context.fill();
+    context.stroke();
+
+
+
+    context.beginPath();
+    context.fillStyle = "PINK";
+    context.rect(150,250,300,100)
+    context.fill();
+    context.stroke();
+
+
+    context.beginPath();
+    context.fillStyle = "PINK";
+    context.rect(250,150,100,100);
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.fillStyle = "PINK";
+    context.rect(450,50,100,200);
+    context.fill();
+    context.stroke();
+
+
+    context.beginPath();
+    context.fillStyle = "PINK";
+    context.rect(450,350,100,100);
+    context.fill();
+    context.stroke();
+
+}
