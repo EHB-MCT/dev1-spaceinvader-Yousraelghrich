bb13ba0f72f598dc855d20155d6bbd4cc426edb5
yousra <yousra.el.ghrich@student.ehb.be>
2022-12-13 15:59:43 +0100

    vierkant en kleur juist

--- spaceInvader.js
+++ spaceInvader.js
@@ -3,50 +3,50 @@
 drawline();
 
 function drawline() {
-    let canvas = document.querySelector("canvas")
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    let context = canvas.getContext("2d");
- 
-    context.beginPath();
-    context.fillStyle = "PINK";
-    context.rect(50,50,100,200);
-    context.fill();
-    context.stroke();
+	let canvas = document.querySelector("canvas");
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	let context = canvas.getContext("2d");
 
+	context.beginPath();
+	context.fillStyle = "black";
+	context.rect(50, 50, 300, 300);
+	context.fill();
+	context.stroke();
 
-    context.beginPath();
-    context.fillStyle = "PINK";
-    context.rect(50,350,100,100);
-    context.fill();
-    context.stroke();
+	context.beginPath();
+	context.fillStyle = "GREEN";
+	context.rect(50, 50, 70, 150);
+	context.fill();
+	context.stroke();
 
+	context.beginPath();
+	context.fillStyle = "GREEN";
+	context.rect(50, 280, 70, 70);
+	context.fill();
+	context.stroke();
 
+	context.beginPath();
+	context.fillStyle = "GREEN";
+	context.rect(120, 200, 160, 80);
+	context.fill();
+	context.stroke();
 
-    context.beginPath();
-    context.fillStyle = "PINK";
-    context.rect(150,250,300,100)
-    context.fill();
-    context.stroke();
+	context.beginPath();
+	context.fillStyle = "GREEN";
+	context.rect(165, 129, 70, 70);
+	context.fill();
+	context.stroke();
 
+	context.beginPath();
+	context.fillStyle = "GREEN";
+	context.rect(280, 50, 70, 150);
+	context.fill();
+	context.stroke();
 
-    context.beginPath();
-    context.fillStyle = "PINK";
-    context.rect(250,150,100,100);
-    context.fill();
-    context.stroke();
-
-    context.beginPath();
-    context.fillStyle = "PINK";
-    context.rect(450,50,100,200);
-    context.fill();
-    context.stroke();
-
-
-    context.beginPath();
-    context.fillStyle = "PINK";
-    context.rect(450,350,100,100);
-    context.fill();
-    context.stroke();
-
+	context.beginPath();
+	context.fillStyle = "GREEN";
+	context.rect(280, 280, 70, 70);
+	context.fill();
+	context.stroke();
 }
